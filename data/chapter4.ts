af87e3a4bc45f8093a532fbb56a936ca70db01a2
Timothy Sullivan <timsullivanaz@gmail.com>
2026-05-29 14:28:11 -0700

    add more chapter4

--- data/chapter4.ts
+++ data/chapter4.ts
@@ -309,4 +309,442 @@ export const Chapter4: CardCollection = {
       ],
     },
   ],
+  'Telling Time': [
+    {
+      subTitle: 'Asking for (and responding about) the time',
+      cards: [
+        {
+          answer: `What time is it?`,
+          question: `¿Qué hora es?`,
+          phrases: [
+            { answer: `It's 3 o'clock`, question: `Son las tres en punto` },
+            { answer: `It's 4:30`, question: `Son las cuatro y media` },
+            { answer: `It's 5:15`, question: `Son las cinco y cuarto` },
+            { answer: `It's 6:45`, question: `Son las siete menos cuarto` },
+            { answer: `It's 7:20`, question: `Son las siete y veinte` },
+            { answer: `It's 8:10`, question: `Son las ocho y diez` },
+            { answer: `It's 9:50`, question: `Son las diez menos diez` },
+          ],
+        },
+        {
+          answer: `It's noon`,
+          question: `Es mediodía`,
+          phrases: [{ answer: `It's midnight`, question: `Es medianoche` }],
+        },
+        {
+          answer: `It's early`,
+          question: `Es temprano`,
+          phrases: [{ answer: `It's early in the morning`, question: `Es temprano en la mañana` }],
+        },
+        {
+          answer: `It's late`,
+          question: `Es tarde`,
+          phrases: [{ answer: `It's late at night`, question: `Es tarde en la noche` }],
+        },
+      ],
+    },
+    {
+      subTitle: 'Times after and before the hour',
+      cards: [
+        {
+          answer: `It's 10:15`,
+          question: `Son las diez y cuarto`,
+          phrases: [{ answer: `It's 11:45 AM`, question: `Son las doce menos cuarto de la mañana` }],
+        },
+        {
+          answer: `It's 2:30`,
+          question: `Son las dos y media`,
+          phrases: [{ answer: `It's 3:30 PM`, question: `Son las tres y media de la tarde` }],
+        },
+        {
+          answer: `It's 4:20`,
+          question: `Son las cuatro y veinte`,
+          phrases: [{ answer: `It's 5:40 AM`, question: `Son las seis menos veinte de la mañana` }],
+        },
+        {
+          answer: `It's 6:10`,
+          question: `Son las seis y diez`,
+          phrases: [{ answer: `It's 7:50 PM`, question: `Son las ocho menos diez de la noche` }],
+        },
+        {
+          answer: `It's 8:45`,
+          question: `Son las nueve menos cuarto`,
+          phrases: [{ answer: `It's 9:45 AM`, question: `Son las diez menos cuarto de la mañana` }],
+        },
+        {
+          answer: `It's 11:55`,
+          question: `Son las doce menos cinco`,
+          phrases: [{ answer: `It's 12:55 PM`, question: `Es la una menos cinco de la tarde` }],
+        },
+      ],
+    },
+    {
+      cards: [
+        {
+          answer: `At what time is...?`,
+          question: `¿A qué hora es...?`,
+          phrases: [
+            { answer: `At 3 o'clock`, question: `A las tres en punto` },
+            { answer: `At 4:30`, question: `A las cuatro y media` },
+            { answer: `At 5:15`, question: `A las cinco y cuarto` },
+            { answer: `At 6:45`, question: `A las siete menos cuarto` },
+            { answer: `At 7:20`, question: `A las siete y veinte` },
+            { answer: `At 8:10`, question: `A las ocho y diez` },
+            { answer: `At 9:50`, question: `A las diez menos diez` },
+          ],
+        },
+      ],
+    },
+    {
+      subTitle: 'Exploring common expressions of time',
+      cards: [
+        {
+          answer: `a second`,
+          question: `un segundo`,
+          phrases: [
+            { answer: `Wait a second`, question: `Espera un segundo` },
+            { answer: `Just a second`, question: `Solo un segundo` },
+          ],
+        },
+        {
+          answer: `a minute`,
+          question: `un minuto`,
+          phrases: [
+            { answer: `Wait a minute`, question: `Espera un minuto` },
+            { answer: `Just a minute`, question: `Solo un minuto` },
+          ],
+        },
+        {
+          answer: `a quarter of an hour`,
+          question: `un cuarto de hora`,
+          phrases: [
+            { answer: `Wait a quarter of an hour`, question: `Espera un cuarto de hora` },
+            { answer: `Just a quarter of an hour`, question: `Solo un cuarto de hora` },
+          ],
+        },
+        {
+          answer: `half an hour`,
+          question: `media hora`,
+          phrases: [
+            { answer: `Wait half an hour`, question: `Espera media hora` },
+            { answer: `Just half an hour`, question: `Solo media hora` },
+          ],
+        },
+        {
+          answer: `in the morning`,
+          question: `por la mañana`,
+          phrases: [
+            { answer: `I have a meeting in the morning`, question: `Tengo una reunión por la mañana` },
+            { answer: `I like to exercise in the morning`, question: `Me gusta hacer ejercicio por la mañana` },
+          ],
+        },
+        {
+          answer: `in the afternoon`,
+          question: `por la tarde`,
+          phrases: [
+            { answer: `I have a class in the afternoon`, question: `Tengo una clase por la tarde` },
+            { answer: `I like to relax in the afternoon`, question: `Me gusta relajarme por la tarde` },
+          ],
+        },
+        {
+          answer: `in the evening`,
+          question: `por la noche`,
+          phrases: [
+            { answer: `I have dinner in the evening`, question: `Ceno por la noche` },
+            { answer: `I like to watch movies in the evening`, question: `Me gusta ver películas por la noche` },
+          ],
+        },
+        {
+          answer: `at night`,
+          question: `en la noche`,
+          phrases: [
+            { answer: `I work at night`, question: `Trabajo en la noche` },
+            { answer: `I like to read at night`, question: `Me gusta leer en la noche` },
+          ],
+        },
+        {
+          answer: `at what time?`,
+          question: `¿a qué hora?`,
+          phrases: [
+            { answer: `At what time is the meeting?`, question: `¿A qué hora es la reunión?` },
+            { answer: `At what time does the movie start?`, question: `¿A qué hora empieza la película?` },
+          ],
+        },
+        {
+          answer: `at exactly 9:00`,
+          question: `a las nueve en punto`,
+          phrases: [{ answer: `The train leaves at exactly 9:00`, question: `El tren sale a las nueve en punto` }],
+        },
+        {
+          answer: `at about 2:00`,
+          question: `a eso de las dos`,
+          phrases: [
+            {
+              answer: `Traffic starts to get bad at about 2:00 PM`,
+              question: `El tráfico empieza a ponerse malo a eso de las dos de la tarde`,
+            },
+          ],
+        },
+        {
+          answer: `in an hour`,
+          question: `en una hora`,
+          phrases: [
+            {
+              answer: `I will be there in an
+          hour`,
+              question: `Estaré allí en una hora`,
+            },
+          ],
+        },
+        {
+          answer: `in a while`,
+          question: `dentro de un rato`,
+          phrases: [
+            {
+              answer: `I will call you
+        in a while`,
+              question: `Te llamaré dentro de un rato`,
+            },
+          ],
+        },
+        {
+          answer: `until 10:00`,
+          question: `hasta las diez`,
+          phrases: [
+            {
+              answer: `I will wait until
+        10:00`,
+              question: `Esperaré hasta las diez`,
+            },
+          ],
+        },
+        {
+          answer: `before 9:00`,
+          question: `antes de las nueve`,
+          phrases: [
+            {
+              answer: `I need to
+        finish this before 9:00`,
+              question: `Necesito terminar esto antes de las nueve`,
+            },
+          ],
+        },
+        {
+          answer: `after 7:00`,
+          question: `después de las siete`,
+          phrases: [
+            {
+              answer: `I will call you
+        after 7:00`,
+              question: `Te llamaré después de las siete`,
+            },
+          ],
+        },
+        {
+          answer: `since what time?`,
+          question: `¿desde qué hora?`,
+          phrases: [
+            {
+              answer: `Since what
+        time have you been waiting?`,
+              question: `¿Desde qué hora has estado esperando?`,
+            },
+          ],
+        },
+        {
+          answer: `since 8:00`,
+          question: `desde las ocho`,
+          phrases: [
+            {
+              answer: `I have been waiting
+        since 8:00`,
+              question: `He estado esperando desde las ocho`,
+            },
+          ],
+        },
+        {
+          answer: `one hour ago`,
+          question: `hace una hora`,
+          phrases: [
+            {
+              answer: `I arrived one hour
+        ago`,
+              question: `Llegué hace una hora`,
+            },
+          ],
+        },
+        {
+          answer: `early`,
+          question: `temprano`,
+          phrases: [{ answer: `I arrived early`, question: `Llegué temprano` }],
+        },
+        {
+          answer: `late`,
+          question: `tarde`,
+          phrases: [{ answer: `I stayed late at the office`, question: `Me quedé tarde en la oficina` }],
+        },
+        {
+          answer: `late (in arriving)`,
+          question: `retrasado`,
+          phrases: [
+            {
+              answer: `I was late to the meeting`,
+              question: `Llegué retrasado a la reunión`,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  'Using the Calendar and Dates': [
+    {
+      subTitle: 'Days of the week',
+      cards: [
+        {
+          answer: 'What day is it today?',
+          question: '¿Qué día es hoy?',
+          phrases: [{ answer: 'Today is Monday', question: 'Hoy es lunes' }],
+        },
+        {
+          answer: 'Monday',
+          question: 'lunes',
+          phrases: [{ answer: 'I have a meeting on Monday', question: 'Tengo una reunión el lunes' }],
+        },
+        {
+          answer: 'Tuesday',
+          question: 'martes',
+          phrases: [{ answer: 'I have a class on Tuesday', question: 'Tengo una clase el martes' }],
+        },
+        {
+          answer: 'Wednesday',
+          question: 'miércoles',
+          phrases: [
+            {
+              answer: 'I have a doctor appointment on Wednesday',
+              question: 'Tengo una cita con el doctor el miércoles',
+            },
+          ],
+        },
+        {
+          answer: 'Thursday',
+          question: 'jueves',
+          phrases: [{ answer: 'I have a party on Thursday', question: 'Tengo una fiesta el jueves' }],
+        },
+        {
+          answer: 'Friday',
+          question: 'viernes',
+          phrases: [{ answer: 'I have a date on Friday', question: 'Tengo una cita el viernes' }],
+        },
+        {
+          answer: 'Saturday',
+          question: 'sábado',
+          phrases: [{ answer: 'I like to relax on Saturday', question: 'Me gusta relajarme el sábado' }],
+        },
+        {
+          answer: 'Sunday',
+          question: 'domingo',
+          phrases: [{ answer: 'I go to church on Sunday', question: 'Voy a la iglesia el domingo' }],
+        },
+      ],
+    },
+    {
+      subTitle: 'Months of the year',
+      cards: [
+        {
+          answer: 'In what month...?',
+          question: '¿En qué mes...?',
+          phrases: [{ answer: 'In what month is your birthday?', question: '¿En qué mes es tu cumpleaños?' }],
+        },
+        {
+          answer: 'January',
+          question: 'enero',
+          phrases: [{ answer: 'My birthday is in January', question: 'Mi cumpleaños es en enero' }],
+        },
+        {
+          answer: 'February',
+          question: 'febrero',
+          phrases: [{ answer: `Valentine's Day is in February`, question: 'El Día de San Valentín es en febrero' }],
+        },
+        {
+          answer: 'March',
+          question: 'marzo',
+          phrases: [{ answer: 'Spring starts in March', question: 'La primavera comienza en marzo' }],
+        },
+        {
+          answer: 'April',
+          question: 'abril',
+          phrases: [
+            { answer: 'April showers bring May flowers', question: 'Las lluvias de abril traen flores de mayo' },
+          ],
+        },
+        {
+          answer: 'May',
+          question: 'mayo',
+          phrases: [{ answer: 'My birthday is in May', question: 'Mi cumpleaños es en mayo' }],
+        },
+        {
+          answer: 'June',
+          question: 'junio',
+          phrases: [{ answer: 'Summer starts in June', question: 'El verano comienza en junio' }],
+        },
+        {
+          answer: 'July',
+          question: 'julio',
+          phrases: [{ answer: 'Independence Day is in July', question: 'El Día de la Independencia es en julio' }],
+        },
+        {
+          answer: 'August',
+          question: 'agosto',
+          phrases: [{ answer: 'I go on vacation in August', question: 'Me voy de vacaciones en agosto' }],
+        },
+        {
+          answer: 'September',
+          question: 'septiembre',
+          phrases: [{ answer: 'School starts in September', question: 'La escuela comienza en septiembre' }],
+        },
+        {
+          answer: 'October',
+          question: 'octubre',
+          phrases: [{ answer: 'Halloween is in October', question: 'Halloween es en octubre' }],
+        },
+        {
+          answer: 'November',
+          question: 'noviembre',
+          phrases: [{ answer: 'Thanksgiving is in November', question: 'El Día de Acción de Gracias es en noviembre' }],
+        },
+        {
+          answer: 'December',
+          question: 'diciembre',
+          phrases: [{ answer: 'Christmas is in December', question: 'La Navidad es en diciembre' }],
+        },
+      ],
+    },
+    {
+      subTitle: 'Seasons of the year',
+      cards: [
+        {
+          answer: 'winter',
+          question: 'invierno',
+          phrases: [{ answer: 'I like to ski in the winter', question: 'Me gusta esquiar en el invierno' }],
+        },
+        {
+          answer: 'spring',
+          question: 'primavera',
+          phrases: [{ answer: 'I like to see the flowers in the spring', question: 'Me gusta ver las flores en la primavera' }],
+        },
+        {
+          answer: 'summer',
+          question: 'verano',
+          phrases: [{ answer: 'I like to go to the beach in the summer', question: 'Me gusta ir a la playa en el verano' }],
+        },
+        {
+          answer: 'fall / autumn',
+          question: 'otoño',
+          phrases: [
+            { answer: 'I like to see the leaves change color in the fall', question: 'Me gusta ver las hojas cambiar de color en el otoño' },
+          ],
+        },
+      ],
+    },
+  ],
 };
