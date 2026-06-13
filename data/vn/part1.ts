908362a5cbb62d2c56087856a82483730acaa319
Timothy Sullivan <timsullivanaz@gmail.com>
2026-06-13 16:14:54 -0700

    more part 1

--- data/vn/part1.ts
+++ data/vn/part1.ts
@@ -51,9 +51,171 @@ export const Part1: CardCollection = {
       { answer: 'We are grateful for your help.', question: 'Chúng con biết ơn sự giúp đỡ của bác.' },
     ] },
   ] }],
-  'Greetings and Partings': [{ cards: [] }],
-  'Introductions': [{ cards: [] }],
-  'Common Questions and Suggested Responses': [{ cards: [] }],
-  'Common Expressions and Phrases': [{ cards: [] }],
-  'Key Words (Titles, Family, Occupations)': [{ cards: [] }],
+  'Greetings and Partings': [{ cards: [
+    // Good morning. / Goodbye. (To a female older than your parents)
+    { answer: 'Good morning. / Goodbye. (To a female older than your parents)', question: 'Chào bà', phrases: [
+      { answer: 'Good morning, ma\'am.', question: 'Chào bà ạ.' },
+    ] },
+    // Good afternoon. / Goodbye. (To a male older than your parents)
+    { answer: 'Good afternoon. / Goodbye. (To a male older than your parents)', question: 'Chào ông', phrases: [
+      { answer: 'Good afternoon, sir.', question: 'Chào ông ạ.' },
+    ] },
+    // Good evening. / Goodbye. (To a female younger than your parents)
+    { answer: 'Good evening. / Goodbye. (To a female younger than your parents)', question: 'Chào cô', phrases: [
+      { answer: 'Good evening, miss.', question: 'Chào cô ạ.' },
+    ] },
+    // Hello. / Goodbye. (To a female peer)
+    { answer: 'Hello. / Goodbye. (To a female peer)', question: 'Chào chị', phrases: [
+      { answer: 'Hello, how are you?', question: 'Chào chị, chị khỏe không?' },
+    ] },
+    // Hello. / Goodbye. (To a male peer)
+    { answer: 'Hello. / Goodbye. (To a male peer)', question: 'Chào anh', phrases: [
+      { answer: 'Hello, how are you?', question: 'Chào anh, anh khỏe không?' },
+    ] },
+    // Hello. / Goodbye. (To a younger person)
+    { answer: 'Hello. / Goodbye. (To a younger person)', question: 'Chào em', phrases: [
+      { answer: 'Hello!', question: 'Chào em!' },
+    ] },
+    // Hello. / Goodbye. (To a person older than your parents)
+    { answer: 'Hello. / Goodbye. (To a person older than your parents)', question: 'Chào bác', phrases: [
+      { answer: 'Hello.', question: 'Chào bác ạ.' },
+    ] },
+    // Hello. / Goodbye. (To a female person younger than your parents)
+    { answer: 'Hello. / Goodbye. (To a female person younger than your parents)', question: 'Chào cô', phrases: [
+      { answer: 'Hello.', question: 'Chào cô ạ.' },
+    ] },
+    // Hello. / Goodbye. (To a male person younger than your parents)
+    { answer: 'Hello. / Goodbye. (To a male person younger than your parents)', question: 'Chào chú', phrases: [
+      { answer: 'Hello.', question: 'Chào chú ạ.' },
+    ] },
+    // Goodbye. (More formal, to a female peer especially if you won't see them for a while)
+    { answer: 'Goodbye. (More formal, to a female peer especially if you won\'t see them for a while)', question: 'Tạm biệt chị', phrases: [
+      { answer: 'Goodbye, take care.', question: 'Tạm biệt chị, giữ gìn sức khỏe nhé.' },
+    ] },
+    // Goodbye. (More formal, to a male peer especially if you won't see them for a while)
+    { answer: 'Goodbye. (More formal, to a male peer especially if you won\'t see them for a while)', question: 'Tạm biệt anh', phrases: [
+      { answer: 'Goodbye, take care.', question: 'Tạm biệt anh, giữ gìn sức khỏe nhé.' },
+    ] },
+    // Goodbye. (More formal, to a younger person especially if you won't see them for a while)
+    { answer: 'Goodbye. (More formal, to a younger person especially if you won\'t see them for a while)', question: 'Tạm biệt em', phrases: [
+      { answer: 'Goodbye, take care.', question: 'Tạm biệt em, giữ gìn sức khỏe nhé.' },
+    ] },
+    // Goodbye. (More formal, to a person older than your parents, especially if you won't see them for a while)
+    { answer: 'Goodbye. (More formal, to a person older than your parents, especially if you won\'t see them for a while)', question: 'Tạm biệt bác', phrases: [
+      { answer: 'Goodbye, take care.', question: 'Tạm biệt bác, bác giữ gìn sức khỏe ạ.' },
+    ] },
+    // Goodbye. (More formal, to a female younger than your parents, especially if you won't see them for a while)
+    { answer: 'Goodbye. (More formal, to a female younger than your parents, especially if you won\'t see them for a while)', question: 'Tạm biệt cô', phrases: [
+      { answer: 'Goodbye, take care.', question: 'Tạm biệt cô, cô giữ gìn sức khỏe ạ.' },
+    ] },
+    // Goodbye. (More formal, to a male younger than your parents, especially if you won't see them for a while)
+    { answer: 'Goodbye. (More formal, to a male younger than your parents, especially if you won\'t see them for a while)', question: 'Tạm biệt chú', phrases: [
+      { answer: 'Goodbye, take care.', question: 'Tạm biệt chú, chú giữ gìn sức khỏe ạ.' },
+    ] },
+    // See you again. (To a female peer)
+    { answer: 'See you again. (To a female peer)', question: 'Hẹn gặp lại chị', phrases: [
+      { answer: 'See you again next week.', question: 'Hẹn gặp lại chị tuần sau.' },
+    ] },
+    // See you again. (To a male peer)
+    { answer: 'See you again. (To a male peer)', question: 'Hẹn gặp lại anh', phrases: [
+      { answer: 'See you again next week.', question: 'Hẹn gặp lại anh tuần sau.' },
+    ] },
+    // See you again. (To a younger person)
+    { answer: 'See you again. (To a younger person)', question: 'Hẹn gặp lại em', phrases: [
+      { answer: 'See you again next week.', question: 'Hẹn gặp lại em tuần sau.' },
+    ] },
+    // See you again. (To a person older than your parents)
+    { answer: 'See you again. (To a person older than your parents)', question: 'Hẹn gặp lại bác', phrases: [
+      { answer: 'See you again next week.', question: 'Hẹn gặp lại bác tuần sau ạ.' },
+    ] },
+    // See you again. (To a female person younger than your parents)
+    { answer: 'See you again. (To a female person younger than your parents)', question: 'Hẹn gặp lại cô', phrases: [
+      { answer: 'See you again next week.', question: 'Hẹn gặp lại cô tuần sau ạ.' },
+    ] },
+    // See you again. (To a male person younger than your parents)
+    { answer: 'See you again. (To a male person younger than your parents)', question: 'Hẹn gặp lại chú', phrases: [
+      { answer: 'See you again next week.', question: 'Hẹn gặp lại chú tuần sau ạ.' },
+    ] },
+  ] }],
+  'Introductions': [{ cards: [
+    // be - là
+    { answer: 'to be', question: 'là', phrases: [
+      { answer: 'I am a teacher.', question: 'Tôi là giáo viên.' },
+    ] },
+    // this, here - dây, đây là
+    { answer: 'this, this is', question: 'đây, đây là', phrases: [
+      { answer: 'This is my friend.', question: 'Đây là bạn của tôi.' },
+    ] },
+    // This is Mrs. Clark
+    { answer: 'This is Mrs. Clark.', question: 'Đây là bà Clark.', phrases: [
+      { answer: 'Mrs. Clark is a teacher.', question: 'Bà Clark là giáo viên.' },
+    ] },
+    // This is Mr. Cruise
+    { answer: 'This is Mr. Cruise.', question: 'Đây là ông Cruise.', phrases: [
+      { answer: 'Mr. Cruise is an actor.', question: 'Ông Cruise là diễn viên.' },
+    ] },
+    // This is Miss Kim
+    { answer: 'This is Miss Kim.', question: 'Đây là cô Kim.', phrases: [
+      { answer: 'Miss Kim is a doctor.', question: 'Cô Kim là bác sĩ.' },
+    ] },
+    // This is Sherry
+    { answer: 'This is Sherry.', question: 'Đây là Sherry.', phrases: [
+      { answer: 'Sherry is my sister.', question: 'Sherry là chị gái của tôi.' },
+    ] },
+    // This is Sean
+    { answer: 'This is Sean.', question: 'Đây là Sean.', phrases: [
+      { answer: 'Sean is my brother.', question: 'Sean là em trai của tôi.' },
+    ] },
+    // name - tên
+    { answer: 'name', question: 'tên', phrases: [
+      { answer: 'What is your name?', question: 'Bạn tên là gì?' },
+    ] },
+    // My name is Mary
+    { answer: 'My name is Mary.', question: 'Tên tôi là Mary.', phrases: [
+      { answer: 'Nice to meet you, Mary.', question: 'Rất vui được gặp bạn, Mary.' },
+    ] },
+    // My name is Tom
+    { answer: 'My name is Tom.', question: 'Tên tôi là Tom.', phrases: [
+      { answer: 'Nice to meet you, Tom.', question: 'Rất vui được gặp bạn, Tom.' },
+    ] },
+    // honored - hân hạnh
+    { answer: 'honored', question: 'hân hạnh', phrases: [
+      { answer: 'I am honored to meet you.', question: 'Tôi hân hạnh được gặp bạn.' },
+    ] },
+    // pleased, happy - vui
+    { answer: 'pleased, happy', question: 'vui', phrases: [
+      { answer: 'I am very happy.', question: 'Tôi rất vui.' },
+    ] },
+    // very - rất
+    { answer: 'very', question: 'rất', phrases: [
+      { answer: 'It is very good.', question: 'Nó rất tốt.' },
+    ] },
+    // Very pleased to meet you. (To a female peer.)
+    { answer: 'Very pleased to meet you. (To a female peer.)', question: 'Rất vui được gặp chị.', phrases: [
+      { answer: 'Very pleased to meet you (female peer) too.', question: 'Tôi cũng rất vui được gặp chị.' },
+    ] },
+    // Very pleased to meet you. (To a male peer.)
+    { answer: 'Very pleased to meet you. (To a male peer.)', question: 'Rất vui được gặp anh.', phrases: [
+      { answer: 'Very pleased to meet you (male peer) too.', question: 'Tôi cũng rất vui được gặp anh.' },
+    ] },
+    // Very pleased to meet you. (To a younger person.)
+    { answer: 'Very pleased to meet you. (To a younger person.)', question: 'Rất vui được gặp em.', phrases: [
+      { answer: 'Very pleased to meet you (the older person) too.', question: 'Em cũng rất vui được gặp anh chị.' },
+    ] },
+    // Very pleased to meet you. (To a person older than your parents.)
+    { answer: 'Very pleased to meet you. (To a person older than your parents.)', question: 'Rất vui được gặp bác.', phrases: [
+      { answer: 'Very pleased to meet you (the much younger person) too.', question: 'Bác cũng rất vui được gặp cháu.' },
+    ] },
+    // Very pleased to meet you. (To a female person younger than your parents.)
+    { answer: 'Very pleased to meet you. (To a female person younger than your parents.)', question: 'Rất vui được gặp cô.', phrases: [
+      { answer: 'Very pleased to meet you (the much younger person) too.', question: 'Cô cũng rất vui được gặp cháu.' },
+    ] },
+    // Very pleased to meet you. (To a male person younger than your parents.)
+    { answer: 'Very pleased to meet you. (To a male person younger than your parents.)', question: 'Rất vui được gặp chú.', phrases: [
+      { answer: 'Very pleased to meet you (the much younger person) too.', question: 'Chú cũng rất vui được gặp cháu.' },
+    ] },
+  ] }],
+  // 'Common Questions and Suggested Responses': [{ cards: [] }],
+  // 'Common Expressions and Phrases': [{ cards: [] }],
+  // 'Key Words (Titles, Family, Occupations)': [{ cards: [] }],
 };
