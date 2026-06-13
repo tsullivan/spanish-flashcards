c01581b9fdf2073e7cfd1bc3093b4ec7fe2c83d8
Timothy Sullivan <timsullivanaz@gmail.com>
2026-06-13 15:22:13 -0700

    starting vietnamese

---
+++ data/es/index.ts
@@ -0,0 +1,20 @@
+import type { AppData } from '../types';
+import { Chapter1 } from './chapter1';
+import { Chapter2 } from './chapter2';
+import { Chapter3 } from './chapter3';
+import { Chapter4 } from './chapter4';
+import { Chapter5 } from './chapter5';
+import { Chapter7 } from './chapter7';
+
+export const source: AppData = {
+  language: 'es-ES',
+  cards: {
+    'Chapter 1 - Spanish in a Nutshell': Chapter1,
+    'Chapter 2 - Warming Up with Spanish Grammar Basics': Chapter2,
+    'Chapter 3 - Getting Started with Basic Expressions': Chapter3,
+    'Chapter 4 - Getting Your Numbers, Times, and Measurements Straight': Chapter4,
+    'Chapter 5 - Speaking Spanish at Home': Chapter5,
+    'Chapter 7 - Asking for Directions and Getting Around': Chapter7,
+  },
+};
+
---
+++ data/vn/index.ts
@@ -0,0 +1,18 @@
+import type { AppData } from '../types';
+import { Part1 } from './part1';
+
+export const source: AppData = {
+  language: 'es-ES',
+  cards: {
+    'Part 1: Common Expressions and Keywords': Part1,
+    // 'Part 2: Numbers, Dates, and Time': Part2,
+    // 'Part 3: Getting Around': Part3,
+    // 'Part 4: Emergencies and Essentials': Part4,
+    // 'Part 5: Out and About': Part5,
+    // 'Part 6: Exploriung Vietnam': Part6,
+    // 'Part 7: Key Names and Signs': Part7,
+    // 'Part 8: The Internet and Social Media': Part8,
+    // 'Part 9: Additional Vocabulary': Part9,
+  },
+};
+
