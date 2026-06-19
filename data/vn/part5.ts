d2215ec74025308fcf64f5d5549b495fa9ef9a5c
Timothy Sullivan <timsullivanaz@gmail.com>
2026-06-19 13:49:37 -0700

    initial part 5

--- data/vn/part5.ts
+++ data/vn/part5.ts
@@ -4,16 +4,452 @@ import type { CardCollection } from '../types';
  * Part 5: Out and About
  */
 export const Part5: CardCollection = {
-  'At the Restaurant': [{ cards: [] }],
-  'At the Coffee Shop': [{ cards: [] }],
-  'Shopping': [{ cards: [] }],
-  'At the Clothing Store': [{ cards: [] }],
-  'At the Bookstore': [{ cards: [] }],
+  'At the Restaurant': [{ cards: [
+    // another - mot...hkac
+    // appetizer - mon khai vi
+    // bring - mang
+    // check (in a restaurant) - giay tinh tien
+    // crabmeat soup - xup cua
+    // delicious - ngon
+    // drink - uong
+    // eat - an
+    // egg roll - cha gio
+    // fried rice - com chien
+    // ice cream - kem
+    // menu - thuc don
+    // order - dat mon
+    // rice noodles - bun
+    // seafood buffet - buffet hai san
+    // soup - xup
+    // more - nua, them
+    // napkin - khan an
+    // orange juice - nuoc cam
+    // order (verb) - goi
+    // pair of chopsticks - doi dua
+    // spoon - thi muoi
+    // table - ban
+    // fork - dia
+    // knife - dao
+    // mess - do an, mon an
+    // paper napkin - khan giay
+    // restaurant (cheap) - tiem an
+    // restaurant (fancy) - nha hang
+    // salty - man
+    // spicy - cay
+    // vegetarian dish - mon chay
+    // water - nuoc
+    // waiter - phuc vu vien
+    // waitress - phuc vu nu
+    // Which restaurant is the best here? - O day nha hang nao ngon nhat?
+    // We'd like a table by the window - Chung toi muon mot ban o gan cua so
+    // Could we have the menu, please? - Cho chung toi xem thuc don voi
+    // Could we have a few more minutes? - Cho chung toi vai phut nua?
+    // We're ready to order now. - Chúng tôi đã sẵn sàng để gọi.
+    // I'll have egg rolls for an appetizer. - Tôi muon goi cha gio lam mon khai vi.
+    // I'll have this vegetarian dish. - Tôi muon goi mon chay nay.
+    // Could I have another pair of chopsticks? - Cho toi xin mot doi dua khac?
+    // I'd like a crabmeat soup and a fried rice. - Cho toi xup cua va com chien.
+    // This soup is too salty. - Xup nay man qua.
+    // Do you have spicy food? - Ban co mon an cay khong?
+    // I can't eat spicy food. - Toi khong an cay duoc.
+    // I'd like a glass of orange juice. - Toi muon mot coc nuoc cam.
+    // Bring me two bottles of water, please. - Mang cho toi hai chai nuoc.
+    // Bring us some more paper napkins, please. - Mang cho chung toi them mot vai khan giay.
+    // Bring me an ice cream, please. - Mang cho toi mot coc kem.
+    // It's so delicious! - Ngon qua!
+    // And an orange juice for me, please. - Va mot ly nuoc cam cho toi.
+    // I'm full. - Toi no roi.
+    // Bring us the check, please. - Mang cho chung toi giay tinh tien.
+  ] }],
+  'At the Coffee Shop': [{ cards: [
+    // black coffee - ca phe den
+    // coffee - ca phe
+    // coffee shop - quan ca phe
+    // cream - sua
+    // espresso - ca phe espresso
+    // latte - ca phe sua
+    // milk - sua
+    // mocha - ca phe mocha
+    // sugar - duong
+    // tea - tra
+    // tea with milk - tra sua
+    // tea with sugar - tra duong
+    // tea with lemon - tra chanh
+    // condensed milk - sua dac
+    // cup - tach, coc
+    // heat up - ham nong, lam nong
+    // ice - da
+    // lukewarm - nguoi, am ap
+    // with - voi
+    // without - khong
+    // croissant - banh mi
+    // biscuit - banh quy
+    // cookie - banh
+    // cheesecake - banh pho mai
+    // coffee cake - banh ca phe
+    // pastry - banh ngot
+    // sandwich - banh mi
+    // I'll have a cup of black coffee with sugar. - Cho toi mot tach ca phe den voi duong.
+    // Could I have a cup of black coffee without sugar, please? - Cho toi mot tach ca phe den khong duong?
+    // I'd like a glass of iced coffee with milk. - Cho toi mot ly ca phe da voi sua.
+    // I'd like a hot coffee with milk. - Cho toi ca phe sua nong.
+    // I'd like more milk. - Cho toi them sua.
+    // My coffee is lukewarm. - Ca phe cua toi hoi nguoi.
+    // Could you heat it up for me? - Ham nong cho toi duoc khong?
+    // Could I have some more sugar, please? - Cho toi xin them chut duong?
+    // Could I have another spoon? - Cho toi xin mot cai thi muoi khac?
+  ] }],
+  'Shopping': [{ cards: [
+    // subTitle: Goods and Produce
+    // banana - trai chuoi
+    // ceramics - do gom
+    // conical hat - non la
+    // embroidered painting - buc tranh theu
+    // gift - qua tang
+    // laquered box - hop son mai
+    // lacquered painting - buc tranh son mai
+    // lacquered vase - binh son mai
+    // mango - trai xoai
+    // oil painting - buc tranh son dau
+    // painting - buc tranh
+    // pineapple - trai thom
+    // silk scarf - khan lua
+    // rambutan - trai chom chom
+    // scarf - khan quang co
+    // sculptured products - hang dieu khac
+    // silk fabric - vai lua
+    // souvenir - qua luu niem
+    // figurine - tuong nho
+    // toy - do choi
+    // coloring book - sach to mau
+    // magazine - tap chi
+    // postcard - buc thu, buc phong su
+    // refrigerator magnet - nam cham tu lanh
+    // magnet - nam cham
+    // eyebrow threading - lam long may
+    // ear piercing - xam khuyen tai
+    // Do you have any souvenirs? - Ban co qua luu niem khong?
+    // Do you have any silk scarves? - Ban co khan lua khong?
+    // I'd like to buy a silk scarf. - Toi muon mua mot cai khan lua.
+    // I'd like to buy some fresh fruit. - Toi muon mua mot vai trai cay tuoi.
+    // Do you do ear piercing? - Ban co xam khuyen tai khong?
+    // Do you do eyebrow threading? - Ban co lam long may khong?
+
+    // subTitle: Purchasing
+    // buy - mua
+    // cheap - re
+    // discount - giam gia
+    // dozen - chuc
+    // beautiful - dep
+    // expensive - mac, dat
+    // fresh - tuoi
+    // good - tot
+    // green, unripe - con xanh
+    // hand-embroidered - theu tay
+    // kilo - ky
+    // look at - xem
+    // market - cho
+    // quality - chat luong
+    // refund - hoan tien
+    // return - tra
+    // ripe - chin
+    // sell - ban
+    // that, there - kia
+    // I'd like to buy some souvenirs. - Toi muon mua vai mon qua luu niem.
+    // How much is this painting? - Buc tranh nay bao nhieu tien?
+    // This is a gift for my mother. - Day la qua tang cho me toi.
+    // I don't want this lacquered box. - Toi khong muon cai hop son mai nay.
+    // I want that one. - Toi muon cai kia.
+    // This lacquered vase is beautiful. How much is it? - Cai binh son mai nay ray dep. Gia bao nhieu?
+    // I want to return this scarf. Here is my receipt. - Toi muon tra cai khan quang co nay. Bien lai cua toi day.
+    // Could I look at that conical hat? - Cho toi xem cai non la kia?
+    // I'd like to buy 2 kilos of rambutans. - Toi muon mua hai ky trai chom chom.
+    // I'd like to buy a dozen mangoes. - Toi muon mua mot chuc xoai.
+    // How much is a pineapple? - Mot trai thom gia bao nhieu?
+    // This one is still green. - Trai nay con xanh.
+    // I'd like to buy some ripe bananas. - Toi muon mua trai chuoi chin.
+
+    // subTitle: Bargaining
+    // come down - bot
+    // more, -er - hon
+    // OK (reluctantly agreeing to a price) - thoi cung duoc
+    // That's much too expensive! - Mac qua!
+    // Won't you come down a little? - Bot mot chut duoc khong?
+    // Come down a little more. - Bot mot chut nua.
+    // Find me a cheaper one. - Tim cho toi mot cai re hon.
+    // OK. Please wrap it up for me. - Thoi cung duoc. Goi cho toi.
+    // How much is that altogether? - Tong cong la bao nhieu?
+  ] }],
+  'At the Clothing Store': [{ cards: [
+    // fit (verb) - vua van
+    // fitting room - phong thu do
+    // pair of high heels - doi giay cao got
+    // pair of shoes - doi giay
+    // pants - quan
+    // shirt - ao so mi
+    // sunglasses - kinh ram
+    // hat - mu, non
+    // tie - ca vat
+    // belt - that lung
+    // socks - tat
+    // necklace - day chuyen
+    // bracelet - lac tay
+    // earrings - hoa tai
+    // watch (noun) - dong ho
+    // skirt - vay dam
+    // ring (noun) - nhan
+    // suit (noun) - bo com le
+    // suit (verb) - hop
+    // try on (clothes) - mac thu
+    // try on (hats) - doi thu
+    // try on (shoes) - mang thu
+    // dress - vay
+    // jeans - quan jean
+    // jacket - ao khoac
+    // blouse - ao nu
+    // coat - ao mang
+    // sweater - ao len
+    // leather - da
+    // soft - mem
+    // fabric - vai
+    // plastic - nhua
+    // I'd like to try on this suit. - Toi muon mac thu bo com le nay.
+    // Do you have a fitting room? - Ban co phong thu do khong?
+    // This shirt doesn't fit me. - Ao so mi nay khong vua toi.
+    // Do you have this jacket in a size medium? - Ban co ao khoac nay size trung khong?
+    // I'd like to try on these shoes. - Toi muon mang thu doi giay nay.
+    // Do you have this skirt in a size small? - Ban co vay dam nay size nho khong?
+    // This dress is too big for me. - Cai vay nay to qua voi toi.
+    // Do you have this sweater in a size large? - Ban co ao len nay size lon khong?
+    // Do you have earrings like these? - Ban co hoa tai nhu the nay khong?
+    // Do you have rings like this? - Ban co nhan nhu the nay khong?
+
+    // subTitle: Colors
+    // beige - mau be
+    // black - mau den
+    // blue - mau xanh duong
+    // brown - mau nau
+    // color - mau
+    // gray - mau xam
+    // green - mau xanh la cay
+    // orange - mau cam
+    // pink - mau hong
+    // purple - mau tim
+    // red - mau do
+    // white - mau trang
+    // yellow - mau vang
+    // What color do you want? - Ban muon mau gi?
+    // I want a red shirt. - Toi muon mot cai ao so mi mau do.
+    // Do you have this coat in white? - Ban co ao mang nay mau trang khong?
+    // I'd like to try on this blue sweater. - Toi muon mac thu cai ao len mau xanh duong nay.
+    // I don't like this pink skirt. - Toi khong thich cai vay dam mau hong nay.
+    // Do you have this hat in green? - Ban co cai mu nay mau xanh la cay khong?
+    // I want to buy a pair of black sunglasses. - Toi muon mua mot doi kinh ram mau den.
+    // I like this purple tie. - Toi thich cai ca vat mau tim nay.
+
+    // subTitle: Measurements and Sizes
+    // big - rong, to
+    // extra-large - lon nhat, cuc lon
+    // height - chieu cao
+    // large - lon
+    // medium - trung
+    // small - nho
+    // size - size, kich co
+    // extra-small - nho nhat, cuc nho
+    // measurement - do luong
+    // meter - met
+    // short - ngan
+    // size - co
+    // weight - trong luong
+    // width - chieu rong
+    // What size do you want? - Ban muon size gi?
+    // These jackets come in three sizes: small, medium, and large. - Nhung chiec ao khoac nay co ba co: nho, trung, va lon.
+    // I want to buy the small one. - Toi muon mua chiec ao nho.
+    // I'd like to try on these pants. - Toi muon mac thu chiec quan nay.
+    // Where's the fitting room? - Phong thu do o dau?
+    // Does this color suit me? - Mau nay co hop toi khong?
+    // How much are these high heels? - Doi giay cao got nay gia bao nhieu?
+    // These shoes are one size too big. Do you have them in a size 8? - Doi giay nay hoi rong. Anh co so tahm khong? // NOTE: a size 8 in the US is a size 39 in Vietnam
+    // These high heels fit perfectly. - Doi giay cao got nay rat vua van.
+    // I always wear brown shoes. - Toi luon mang giay mau nau.
+    // I like the gray suit. - Toi thich bo com le mau xam.
+    // I don't like the green shirt. - Toi khong thich chiec ao so mi mau xanh la cay.
+    // Give me a discount on this sweater, will you? - Giam gia chiec ao len nay cho toi, duoc khong?
+  ] }],
+  'At the Bookstore': [{ cards: [
+    // author - tac gia
+    // book - cuon sach, sach
+    // culture - van hoa
+    // dictionary - tu dien
+    // English-Vietnamese dictionary - tu dien Anh-Viet
+    // help - giup
+    // history - lich su
+    // in (a certain language) - bang
+    // magazine - tap chi
+    // map - ban do
+    // novel - tieu thuyet
+    // publisher - nha xuat ban
+    // reference book - sach tham khao
+    // travel guide - sach huong dan du lich
+    // Vietnamese-English dictionary - tu dien Viet-Anh
+    // newspaper - bao
+    // novelist - nha van
+    // painting - buc tranh
+    // picture book - truyen tranh
+    // pack of playing cards - bo bai
+    // pack of Pokemon cards - bo bai Pokemon
+    // postcard - buc thu, buc phong su
+    // bookmark - dau sach
+    // sticker - nhan dan, tem dan
+    // headphones - tai nghe
+    // learn, study - hoc
+    // cafe - quan ca phe
+    // croissant - banh mi
+    // comic book - truyen tranh
+    // cheesecake - banh pho mai
+    // coffee cake - banh ca phe
+    // focus - tap trung
+    // I want to buy an English-Vietnamese dictionary. - Toi muon mua mot cuon tu dien Anh-Viet.
+    // I will teach myself Vietnamese. - Toi se tu hoc tieng Viet.
+    // Do you have any Pokemon cards? - Ban co bo bai Pokemon khong?
+    // Where are the postcards? - Ban do buc thu o dau?
+    // I'd like to buy this novel. - Toi muon mua cuon tieu thuyet nay.
+    // Do you have any books about Vietnamese culture? - Ban co sach ve van hoa Viet Nam khong?
+    // I'd like to buy a pack of playing cards. - Toi muon mua mot bo bai.
+    // Let's look at the comics. - Hay xem truyen tranh di.
+    // I'm interested in Vietnamese history. - Toi quan tam den lich su Viet Nam.
+    // Please hep me find a book on Vietnamese culture. - Lam on giup toi tim mot cuon sach ve van hoa Viet Nam.
+    // I want an English-language newspaper. - Toi muon mot to bao tieng Anh.
+  ] }],
   'At the Post Office': [{ cards: [] }],
   'At the Beauty Salon': [{ cards: [] }],
   'At the Barbershop': [{ cards: [] }],
-  'Visiting a Vietnamese Home': [{ cards: [] }],
-  'Small Talk (Seasons, Weather, etc.)': [{ cards: [] }],
-  'Common Locations and Business': [{ cards: [] }],
-  'Forms of Entertainment': [{ cards: [] }],
+  'Visiting a Vietnamese Home': [{ cards: [
+    // antique chair - ghe co
+    // antique table - ban co
+    // dining room - phong an
+    // fragrant - thom
+    // living room - phong khach
+    // must be - chac la
+    // priceless -vo gia
+    // spacious - rong
+    // take off - coi
+    // toilet - nha ve sinh
+    // kitchen - bep
+    // bedroom - phong ngu
+    // door - cua
+    // window - cua so
+    // rug - tham
+    // painting (noun) - buc tranh
+    // wall - tuong
+    // silverware - do an bang bac
+    // cup - coc
+    // vase - binh
+    // Do we need to take off our shoes? - Chung toi co can coi giay ra khong?
+    // Your house is beautiful! - Nha cua anh chi dep qua!
+    // Your living room is so spacious! - Phong khach cua anh chi rong qua!
+    // This antique table must be priceless. - Chiec ban co nay chac la vo gia.
+    // Could I use the toilet? - Toi dung nha ve sinh duoc khong?
+    // This tea is so fragrant! - Tra nay thom qua!
+    // Where did you buy it? Anh chi mua no o dau?
+    // We are so sorry we cannot stay for dinner. - Chung toi xin loi khong the o lai cho bua toi duoc.
+    // Thank you for inviting us. - Cam on anh chi da moi chung toi.
+    // Your bedroom is so beautiful! - Phong ngu cua anh chi dep qua!
+  ] }],
+  'Small Talk (Seasons, Weather, etc.)': [{ cards: [
+    // again - lai
+    // always - luon luon
+    // beautiful - dep
+    // because - vi
+    // cloud - may
+    // cloudy - nhieu may
+    // cold - lanh
+    // cold season - mua lanh
+    // cool - mat
+    // dry - kho
+    // dry season - mua kho
+    // fall (noun) - mua thu
+    // hot - nong
+    // hot season - mua nong
+    // humid - am
+    // love (verb) - yeu
+    // rain - mua
+    // rainy season - mua mua
+    // season - mua
+    // ski - truot tuyet
+    // snow - tuyet
+    // snowy - co tuyet
+    // spring (noun) - mua xuan
+    // summer (noun) - mua he
+    // sunny - nang
+    // throughout, all of - suot
+    // usually - thuong
+    // weather - troi
+    // weather forecast - du bao thoi tiet
+    // winter (noun) - mua dong
+    // prepare - chuan bi
+    // Do you like the weather here? - Ban co thich thoi tiet o day khong?
+    // It's so hot today! - Hom nay nong qua!
+    // It's so cool tonight. - Toi nay troi mat qua!
+    // It's raining again! - Troi lai mua!
+    // We hate rainy weather. - Chúng tôi ghét thời mưa.
+    // I like sunny weather. - Toi thich troi nang.
+    // I love the spring because the weather is always beautiful. - Toi yeu mua xuan vi thoi tiet luon dep.
+    // What's your favorite season? - Anh/Chi thich mua nao nhat?
+    // Summer's my favorite season. - Toi thich mua he nhat.
+    // In summer, the days are longer and it's usually sunny. - Vao mua he, ngay dai hon va thuong nang.
+    // According to the weather forecast, it will rain all week next week. - Theo du bao thoi tiet, troi se mua suot tuan sau.
+    // What do you enjoy doing in the winter? - Anh/Chi thich lam gi vao mua dong?
+    // I like to go skiing. - Toi thich di truot tuyet.
+    // I don't like cold weather. - Toi khong thich thoi tiet lanh.
+  ] }],
+  'Common Locations and Business': [{ cards: [
+    // airport - phi truong, san bay
+    // bank - ngan hang
+    // bar - quan ruou
+    // barbershop - tiem hot toc, tiem cat toc
+    // beach - bai bien
+    // beauty salon - tham my vien, tiem lam dep
+    // shop (noun) - cua hang
+    // bookstore - nha sach
+    // church - nha tho
+    // dance club - vu truong
+    // hospital - benh vien
+    // library - thu vien
+    // market, marketplace - cho
+    // pagoda, temple - chua
+    // park - cong vien
+    // post office - buu dien
+    // school - truong hoc
+    // supermarket - sieu thi
+    // university - dai hoc
+    // zoo - so thu
+    // gas station - cay xang
+    // restaurant - nha hang, tiem an
+    // Where is the nearest bank? - Ngan hang gan nhat o dau?
+    // Let's go to a beauty salon. - Hay di den tham my vien di.
+  ] }],
+  'Forms of Entertainment': [{ cards: [
+    // coffee shop - quan ca phe
+    // free - ranh, mien phi
+    // fun - vui
+    // ice-cream parlor - tiem kem, quan kem
+    // karaoke bar - quan karaoke
+    // let us - chung ta...di
+    // movie theater - rap chieu phim, xi-ne // NOTE: xi-ne is a loanword from French, but it's commonly used in Vietnamese to refer to movie theaters
+    // music - am nhac
+    // nightclub - quan bar, vu truong, hop dem
+    // opera house - nha hat lon
+    // together - cung, cung nhau
+    // weekend - cuoi tuan
+    // I want to go have an ice cream. - Toi muon di an kem.
+    // I want to go see a movie. - Toi muon di xem xi-ne.
+    // I want to go have a coffee. - Toi muon di uong ca phe.
+    // I have 3 tickets to the opera house this weekend. - Toi co ba ve nha hat lon vao cuoi tuan nay.
+    // Let's go find a karaoke bar. - Chung ta di tim mot quan karaoke di.
+    // We're going to a nightclub. - Chung toi dang di den mot hop dem.
+    // Are you free, Hoa? - Anh Hoaa ca ranh khong?
+    // Do you want to come with us, Kim? - Chi Kim co muon di cung chung toi khong?
+    // Let's go together. It will be fun! - Chung ta cung di di. Vui lam!
+  ] }],
 };
