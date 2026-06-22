f06d22b0a7380f65c92dab5103ad221662c64794
Timothy Sullivan <timsullivanaz@gmail.com>
2026-06-21 17:06:55 -0700

    spanish-flashcards

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vietnamese-flashcards/',
+  base: '/spanish-flashcards/',
   plugins: [
     vue(),
     vueDevTools(),
